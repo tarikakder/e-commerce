a3c78f1e171b110a152f21f86a87017d48767be4
tarik <tarikakdere@gmail.com>
2022-07-04 16:56:38 +0300

    laravel e-commerce fourth-push mainpage add

--- database/migrations/2014_10_12_000000_create_order_table.php
+++ database/migrations/2014_10_12_000000_create_order_table.php
@@ -13,15 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('surname');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('transactionProd');
+            $table->string('address');
+            $table->dateTime('orderDate');
+            $table->integer('quantity_Order');
         });
     }
 
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('orders');
     }
 };
--- database/migrations/2014_10_12_000000_create_products_table.php
+++ database/migrations/2014_10_12_000000_create_products_table.php
@@ -13,15 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('surname');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('transactionCode');
+            $table->string('productName');
+            $table->string('price');
+            $table->integer('quantity');
         });
     }
 
@@ -32,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('products');
     }
 };
