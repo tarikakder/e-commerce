ae412759221a04881b4b3b588e507cbf047d005a
tarik <tarikakdere@gmail.com>
2022-07-26 10:50:20 +0300

    E-commerce Platform First Commit

--- app/Http/Controllers/basketController.php
+++ app/Http/Controllers/basketController.php
@@ -10,12 +10,12 @@ use App\Models\ProductModel;
 class basketController extends Controller
 {
     public function index(){
-        return view('baskett');
+        return view('basket');
     }
 
-    public function addd(Request $request){
+    public function addd(){
         $product=ProductModel::find(\request('id'));
-        \Cart::add($product->id,$product->product_name,1,$product->price);
+        \Cart::add($product->id,$product->product_name,1,$product->product_amount);
 //        \Cart::add([
 //            'id'=>$request->id,
 //            'product_name'=>$request->product_name,
